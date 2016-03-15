1eb5912a60017bdbfa6730430f2c2e72eb3e89f4
Emmanouhl Stergiadis <steremma@ui3.afroditi.hellasgrid.gr>
2016-03-15 17:45:07 +0200

    purged jobs are treated as cleared

--- createBBH/currentState.sh
+++ createBBH/currentState.sh
@@ -6,8 +6,10 @@ running=0
 scheduled=0
 cleared=0
 aborted=0
-
+unknown=0
+sum=0
 for i in `ls`; do
+	((sum++))
 	status=$(echo 2 | glite-wms-job-status -i $i | grep "Current")
 	if [[ $status == *"Running"* ]]; then
 		((running++))
@@ -17,16 +19,22 @@ for i in `ls`; do
 		((cleared++))
 	elif [[ $status = *"Aborted"* ]]; then
 		((aborted++))
+		echo "job $i was aborted"
 	else
-		echo "$status"
+		status=$(echo 2 | glite-wms-job-status -i $i | grep "purged")
+		if [[ ! -z $status ]]; then
+			((cleared++))
+		else 
+			((other++))
+		fi
 	fi
 done
 
-sum=`expr $running + $scheduled + $cleared + $aborted`
 cd ../..
 
 echo "There are $running jobs running!"
 echo "There are $scheduled jobs scheduled!"
 echo "There are $cleared jobs cleared!"
 echo "There are $aborted jobs aborted!"
-echo "In summary i checked $sum jobs!"
+echo "There are $other jobs with unknown status!"
+echo "There are $sum jdl files in the JOBID/ directory!"
